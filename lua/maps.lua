40c53bcc45832911796f754ff9ad704312b4e437
wonderful <390969468@qq.com>
2022-11-09 15:19:20 +0800

    good

--- lua/maps.lua
+++ lua/maps.lua
@@ -3,11 +3,20 @@ vim.g.mapleader = ' '
 local keymap = vim.keymap
 
 keymap.set('n','<C-a>','gg<S-v>G')
+keymap.set('n','+','<C-a>')
+keymap.set('n','-','<C-x>')
+
 keymap.set('i','jj','<ESC>')
 keymap.set('n','<Leader>q',':q<CR>')
 keymap.set('n','<Leader>w',':w<CR>')
 keymap.set('n','<C-s>',':w<CR><Leader>fm')
 keymap.set('i','<C-s>','<ESC>:w<CR>')
+keymap.set('n','H','^')
+keymap.set('n','L','$')
+keymap.set('i','<C-h>','<Left>')
+keymap.set('i','<C-j>','<Down>')
+keymap.set('i','<C-k>','<Up>')
+keymap.set('i','<C-l>','<Right>')
 
 keymap.set('n','ss',':vsplit<Return><C-w>w',{silent = true})
 keymap.set('n','<Leader>rc',':vsplit ~/.config/nvim/init.lua<Return>',{silent = true})
